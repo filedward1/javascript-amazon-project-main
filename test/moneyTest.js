0f1b9de9486a9a87992c7afbbfbb525c22b4018a
fil <filedwardb@gmail.com>
2025-01-30 18:26:33 +0800

    Create automated tests for formatCurrency

---
+++ test/moneyTest.js
@@ -0,0 +1,27 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
+console.log('test suite : formatCurrency');
+
+console.log('converts cents into dollars ');
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('Test passed');
+} else {
+    console.error('Test failed');
+}
+
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00') {
+    console.log('Test passed');
+  } else {
+      console.error('Test failed');
+  }
+
+console.log('rounds up to the nearesrt cent');
+
+  if (formatCurrency(2000.5) === '20.01') {
+    console.log('Test passed');
+  } else {
+      console.error('Test failed');
+  }
